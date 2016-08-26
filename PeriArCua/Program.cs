e89716e44bf284ca84de2fde4841793640a0328e
AndFueHer <crazyz.224@gmail.com>
2016-08-25 19:41:17 -0600

    Calculo del area y perimetro del Triangulo mas el tipo

---
+++ PeriArCua/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeriArCua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string duh="";
+            while (duh != "No") {
+                Console.Clear();
+                Console.WriteLine("Sistema para calcular Area y Perimetro de un triangulo\nAdemas que tipo es\n");
+                Console.WriteLine("Calculo del Area");
+                Triangulo triang = new Triangulo();
+
+                Console.WriteLine("Ingrese la Base:");
+                triang.Base = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la Altura:");
+                triang.Altura = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("El area del triangulo es: " + triang.Area());
+                Console.ReadKey();
+
+                Console.WriteLine("\nCalculo del Perimetro, ademas da el tipo de triangulo");
+                Console.WriteLine("Ingrese el primer valor:");
+                triang.Lado1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el segundo valor:");
+                triang.Lado2 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("E ingrese el tercer valor:");
+                triang.Lado3 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("El Perimetro del triangulo es: " + triang.Perimetro());
+                Console.ReadKey();
+                Console.WriteLine(triang.Verificacion());
+                Console.ReadKey();
+                Console.WriteLine("Desea continuar? Si o No?");
+                duh = Console.ReadLine();
+            }
+            Console.Clear();
+            Console.WriteLine("\n\n\n\n\n            CHAO!!!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PeriArCua/Triangulo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeriArCua
+{
+    class Triangulo
+    {
+        private double base1;
+        private double altura;
+        private int lado1;
+        private int lado2;
+        private int lado3;
+
+        public Triangulo()
+        {
+        }
+        public double Base
+        {
+            get { return base1; }
+            set { base1 = value; }
+        }
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+        public int Lado1
+        {
+            get { return lado1; }
+            set { lado1 = value; }
+        }
+        public int Lado2
+        {
+            get { return lado2; }
+            set { lado2 = value; }
+        }
+        public int Lado3
+        {
+            get { return lado3; }
+            set { lado3 = value; }
+        }
+
+        public double Area()
+        {
+            double resul = (base1 * altura) / 2;
+            return resul;
+        }
+
+        public int Perimetro()
+        {
+            int result = lado1 + lado2 + lado3;
+            return result;
+        }
+
+        public string Verificacion()
+        {
+            string value;
+            if ((lado1 == lado2) && (lado2 == lado3) && (lado1 == lado3))
+            {
+                value= "El triangulo es EQUILATERO";
+            }
+            else if ((lado1 == lado2) || (lado1 == lado3) || (lado2 == lado3))
+            {
+                value= "El triangulo es ISOCELES";
+            }
+            else
+            {
+                value= "El triangulo es ESCALENO";
+            }
+            return value;
+        }
+    }
+}
